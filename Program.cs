046ee95cb5599c3224c9d6f5c24a1fef42c6d6a1
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-04-24 10:53:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        delegate double Operation(double a, double b);
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Operation sum = new Operation(Sum);
+            Operation subtract = new Operation(Subtract);
+            Operation multiply = new Operation(Multiply);
+            Operation divide = new Operation(Divide);
+            double num1 = 10;
+            double num2 = 5;
+            Console.WriteLine($"Сумма: {sum(num1, num2)}");
+            Console.WriteLine($"Разность: {subtract(num1, num2)}");
+            Console.WriteLine($"Произведение: {multiply(num1, num2)}");
+            Console.WriteLine($"Деление: {divide(num1, num2)}");
+            Console.Read();
+
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+
+        public static double Divide(double a, double b)
+        {
+
+            if (b == 0)
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return 0;
+            }
+            return a / b;
+        }
+    }
+}
