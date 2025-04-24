036fbab6806ca60ff6756b9aa4b9ab411a4fb035
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-04-24 10:42:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        delegate void Message();//Обьявляем delegat
+
+        static void Main(string[] args)
+        {
+            Message message;//Создаем обьект делегата и связываем
+            if (DateTime.Now.Hour < 12)
+                message = GoodMorning;
+            else
+                message = GoodEvening;
+            message();
+            Console.Read();
+
+        }
+        
+
+            
+        public static void GoodMorning()
+        {
+            Console.WriteLine("Доброе утро!");
+        }
+        public static void GoodEvening()
+        {
+            Console.WriteLine("Добрый вечер!");
+        }
+    }
+}
